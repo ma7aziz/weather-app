693d7937ed3a37531dac55079ccc39cc4accdd68
mahmoud <ma7.aelaziz@gmail.com>
2020-03-09 14:59:31 +0200

    fix mixed content

--- forecast.js
+++ forecast.js
@@ -2,7 +2,7 @@ const key = 'rVcBAMA6AxmA86BsCth4OxHwoxmLQ0GI'
 
 // get city data
 const getCity = async (city) => {
-    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search'
+    const base = 'https://dataservice.accuweather.com/locations/v1/cities/search'
     const query = `?apikey=${key}&q=${city}`
     const response = await fetch(base + query)
     const data = await response.json()
@@ -12,7 +12,7 @@ const getCity = async (city) => {
 
 //get weather 
 const getWeather = async (id) => {
-    const base = `http://dataservice.accuweather.com/currentconditions/v1/${id}`
+    const base = `https://dataservice.accuweather.com/currentconditions/v1/${id}`
     const query = `?apikey=${key}`
     const response = await fetch(base + query)
     const data = await response.json()
@@ -24,12 +24,12 @@ const getLocation = async () => {
         navigator.geolocation.getCurrentPosition(   async (position)=>{
         const lat =  position.coords.latitude
         const long = position.coords.longitude
-        const response = await fetch(`http://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=${key}&q=${lat},${long}`)
+        const response = await fetch(`https://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=${key}&q=${lat},${long}`)
         const data = await response.json()
         const citykey = await data.Key
         const cityEng = await data.EnglishName
         const country = await data.Country.ID
-        const weatherRes = await fetch(`http://dataservice.accuweather.com/currentconditions/v1/${citykey}?apikey=${key}`)
+        const weatherRes = await fetch(`https://dataservice.accuweather.com/currentconditions/v1/${citykey}?apikey=${key}`)
         const weatherData = await weatherRes.json()
         const weather = await weatherData[0]
         const temp = await weather.Temperature.Metric.Value
